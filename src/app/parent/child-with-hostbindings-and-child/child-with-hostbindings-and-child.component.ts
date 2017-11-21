e6336a4225788600230ca87a2eaf0523935b5925
mgmarlow <Graham.Marlow@socreate.it>
2017-11-21 09:58:12 -0800

    extend hostbinding example with abstract class implementation

--- src/app/parent/child-with-hostbindings-and-child/child-with-hostbindings-and-child.component.ts
+++ src/app/parent/child-with-hostbindings-and-child/child-with-hostbindings-and-child.component.ts
@@ -7,14 +7,28 @@ export enum ChildHostBindingLayout {
   Blue
 }
 
+abstract class LayoutInputComponent {
+  layout: any;
+
+  _parentLayout: any;
+  @Input() set parentLayout(parent: any) {
+    this._parentLayout = parent;
+    this.layout = this.getLayoutFromParent(parent);
+  }
+
+  abstract getLayoutFromParent(parent: any): any;
+}
+
 @Component({
   selector: 'app-child-with-hostbindings-and-child',
   templateUrl: './child-with-hostbindings-and-child.component.html'
 })
-export class ChildWithHostbindingsAndChildComponent {
-  _parentLayout: ParentLayout;
-  @Input() set parentLayout(parent) {
-    this._parentLayout = parent;
+export class ChildWithHostbindingsAndChildComponent extends LayoutInputComponent {
+  @HostBinding('class.red') get red () { return this._parentLayout === ParentLayout.Layout01; }
+  @HostBinding('class.green') get green () { return this._parentLayout === ParentLayout.Layout02; }
+  @HostBinding('class.blue') get blue () { return this._parentLayout === ParentLayout.Layout03; }
+
+  getLayoutFromParent(parent: ParentLayout): ChildHostBindingLayout {
     switch (parent) {
       case ParentLayout.Layout01:
         this.layout  = ChildHostBindingLayout.Red;
@@ -26,11 +40,7 @@ export class ChildWithHostbindingsAndChildComponent {
         this.layout  = ChildHostBindingLayout.Blue;
         break;
     }
+    return this.layout;
   }
-
-  @HostBinding('class.red') get red () { return this._parentLayout === ParentLayout.Layout01; }
-  @HostBinding('class.green') get green () { return this._parentLayout === ParentLayout.Layout02; }
-  @HostBinding('class.blue') get blue () { return this._parentLayout === ParentLayout.Layout03; }
-
-  layout: ChildHostBindingLayout;
 }
+
